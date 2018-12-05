ead7eacf4ba7343a9fc137ef6f95dc73370e5b6e
Krzysztof Królikowski <kkrolikowski@gmail.com>
2018-12-05 14:22:40 +0100

    PoC
    Program connects to local network via wifi. Gets temperature and humidity from sensor and sends data via HTTP to remote backend server

---
+++ enviro.ino
@@ -0,0 +1,97 @@
+#include <dht11.h>
+#include <ESP8266WiFi.h>
+
+/***************************************** Inicjalizacja Sprzetu ********************************/
+
+
+// sensor settings
+dht11 sensor;
+#define SENSOR_PIN 2
+
+// wifi settings
+const char *ssid = "wifi_ssid";
+const char *pass = "wifi_pass";
+
+// stats collector
+const char *server = "backend.server.name";
+WiFiClient http;
+
+void setup() 
+{
+	Serial.begin(115200);
+  delay(100);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to: ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, pass);
+  while( WiFi.status()  != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Wifi connected");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() 
+{
+	int sensStatus = sensor.read(SENSOR_PIN);
+  String httphdrs, httpReq;
+	Serial.print("Sensor status: ");
+	switch (sensStatus)
+	{
+	case DHTLIB_OK:
+		ConsolePrint(sensor);
+    httphdrs = buildHTTPHeaders();
+    httpReq = buildHTTPReq(sensor);
+     if ( http.connect(server, 80))
+     {
+      Serial.println("HTTP server connected");
+      http.println(httpReq);
+      http.println(httphdrs);
+      http.println();
+     }
+		break;
+	case DHTLIB_ERROR_CHECKSUM:
+		Serial.print("Sensor data CHECKSUM ERROR\n");
+		break;
+	case DHTLIB_ERROR_TIMEOUT:
+		Serial.print("Sensor timeout\n");
+		break;
+	default:
+		Serial.print("Unknown ERROR\n");
+		break;
+	}
+
+	delay(1000);
+}
+void ConsolePrint(dht11 data)
+{
+	Serial.print("Humidity (%): ");
+	Serial.print((float)data.humidity, 2);
+	Serial.print("\t\t");
+	Serial.print("Temperature (C): ");
+	Serial.println((float)data.temperature, 2);
+}
+String buildHTTPReq(dht11 data)
+{
+  String httpreq = "GET /collect.php?temp=";
+  httpreq += String((float)data.temperature, 2);
+  httpreq += "&hum=";
+  httpreq += String((float)data.humidity,2);
+  httpreq += " HTTP/1.0";
+  
+  return httpreq;
+}
+String buildHTTPHeaders()
+{
+  String hdr = "Host: ";
+  hdr += server;
+
+  return hdr;
+}
+
