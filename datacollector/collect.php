a20cd57b51439bcbe7a041d1cf78b209ac142593
Krzysztof Królikowski <kkrolikowski@gmail.com>
2018-12-06 10:45:28 +0100

    sensor data collector

---
+++ datacollector/collect.php
@@ -0,0 +1,52 @@
+<?php
+    include_once './db.inc.php';
+
+    $dsn = 'mysql:host='.DB_HOST.';dbname='.DB_NAME;
+    $dbh = new PDO($dsn, DB_USER, DB_PASS);
+    
+    // timestamp
+    
+    $time = new DateTime();
+    $timestamp = $time->getTimestamp();
+
+    // client data
+
+    $data = array();
+    foreach ($_GET as $param => $value) {
+        if (isset($param)) {
+            $data[$param] = $value;
+        }
+    }
+
+    // data validation
+    $mandatory = array('temp', 'sensor_id', 'hum');
+    $valid_array = 1;
+
+    foreach ($mandatory as $param) {
+        if (!array_key_exists($param, $data)) {
+            $valid_array = 0;
+        }
+    }
+
+    // collect data
+
+    if ($valid_array) {
+        $insert_data = "INSERT INTO sensor_data(sensor_id, temp, hum, timestamp) ".
+                        " VALUES('".$data['sensor_id']."', '".$data['temp']."', '".
+                        $data['hum'].", '".$timestamp."')";
+        
+        print $insert_data;
+        $query = $dbh->prepare("INSERT INTO sensor_data(`sensor_id`,`temp`,`hum`,`timestamp`) VALUES(?,?,?,?)");
+        $query->bindParam(1, $data['sensor_id']);
+        $query->bindParam(2, $data['temp']);
+        $query->bindParam(3, $data['hum']);
+        $query->bindParam(4, $timestamp);
+        
+        $query->execute();
+
+    }
+    else {
+        print "Incoplete data, check sensor!";
+    }
+
+?>
